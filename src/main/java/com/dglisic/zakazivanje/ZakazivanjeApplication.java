9bf322217c708a0775ebd5815d039c1e2eb7df49
dglisic <dimitrije.glisic.ext@riag-digital.com>
2023-05-20 19:44:48 +0200

    Initial commit

---
+++ src/main/java/com/dglisic/zakazivanje/ZakazivanjeApplication.java
@@ -0,0 +1,13 @@
+package com.dglisic.zakazivanje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakazivanjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZakazivanjeApplication.class, args);
+	}
+
+}
