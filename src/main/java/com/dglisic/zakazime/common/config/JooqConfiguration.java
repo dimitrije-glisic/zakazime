e33279e883e9a85371d7a4815fd87a2c20ab74a3
Dimitrije Glisic <dimitrije.glisic.ext@riag-digital.com>
2023-12-28 21:57:54 +0100

    Declare jOOQ DAOs as Spring Beans

--- src/main/java/com/dglisic/zakazime/common/config/JooqConfiguration.java
+++ src/main/java/com/dglisic/zakazime/common/config/JooqConfiguration.java
@@ -1,5 +1,12 @@
 package com.dglisic.zakazime.common.config;
 
+import jooq.tables.daos.BusinessAccountMapDao;
+import jooq.tables.daos.BusinessDao;
+import jooq.tables.daos.BusinessTypeDao;
+import jooq.tables.daos.RoleDao;
+import jooq.tables.daos.ServiceCategoryDao;
+import jooq.tables.daos.ServiceDao;
+import jooq.tables.daos.ServiceSubcategoryDao;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
@@ -11,8 +18,8 @@ import org.springframework.context.annotation.Configuration;
 public class JooqConfiguration {
 
   @Bean
-  public DefaultConfiguration configuration(DataSourceConnectionProvider connectionProvider) {
-    DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
+  public DefaultConfiguration configuration(final DataSourceConnectionProvider connectionProvider) {
+    final DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
     jooqConfiguration.setSQLDialect(SQLDialect.POSTGRES);
     jooqConfiguration.set(connectionProvider);
 
@@ -20,8 +27,47 @@ public class JooqConfiguration {
   }
 
   @Bean
-  public DefaultDSLContext dsl(DefaultConfiguration configuration) {
+  public DefaultDSLContext dsl(final DefaultConfiguration configuration) {
     return new DefaultDSLContext(configuration);
+  }
+
+  // ===================================================================================================================
+  // DAO beans for each table
+  // ===================================================================================================================
+
+  @Bean
+  public BusinessAccountMapDao businessAccountMapDao(final DefaultConfiguration configuration) {
+    return new BusinessAccountMapDao(configuration);
+  }
+
+  @Bean
+  public BusinessDao businessDao(final DefaultConfiguration configuration) {
+    return new BusinessDao(configuration);
+  }
+
+  @Bean
+  public BusinessTypeDao businessTypeDao(final DefaultConfiguration configuration) {
+    return new BusinessTypeDao(configuration);
+  }
+
+  @Bean
+  public RoleDao roleDao(final DefaultConfiguration configuration) {
+    return new RoleDao(configuration);
+  }
+
+  @Bean
+  public ServiceCategoryDao serviceCategoryDao(final DefaultConfiguration configuration) {
+    return new ServiceCategoryDao(configuration);
+  }
+
+  @Bean
+  public ServiceDao serviceDao(final DefaultConfiguration configuration) {
+    return new ServiceDao(configuration);
+  }
+
+  @Bean
+  public ServiceSubcategoryDao serviceSubcategoryDao(final DefaultConfiguration configuration) {
+    return new ServiceSubcategoryDao(configuration);
   }
 
 }
