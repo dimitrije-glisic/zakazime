1d0aebe255780696d89dcf23e88d81863f964c7e
Dimitrije Glisic <dimitrije.glisic.ext@riag-digital.com>
2024-01-17 21:41:40 +0100

    Implement image upload for business types

--- src/main/java/com/dglisic/zakazime/business/controller/BusinessTypeController.java
+++ src/main/java/com/dglisic/zakazime/business/controller/BusinessTypeController.java
@@ -3,11 +3,18 @@ package com.dglisic.zakazime.business.controller;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import com.dglisic.zakazime.business.controller.dto.CreateBusinessTypeRequest;
+import com.dglisic.zakazime.business.controller.dto.ImageUploadResponse;
 import com.dglisic.zakazime.business.controller.dto.UpdateBusinessTypeRequest;
 import com.dglisic.zakazime.business.service.BusinessTypeService;
 import com.dglisic.zakazime.common.MessageResponse;
 import jakarta.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.Objects;
 import jooq.tables.pojos.BusinessType;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -19,7 +26,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("business-types")
@@ -63,6 +72,18 @@ public class BusinessTypeController {
     logger.info("Deleting business type with id {}", id);
     businessTypeService.delete(id);
     return new MessageResponse("Business type deleted successfully");
+  }
+
+  final static String UPLOAD_DIRECTORY = "C:\\Users\\dglisic\\personal-projects\\storage\\images\\";
+
+  @PostMapping(value = "/upload-image", consumes = {"multipart/form-data"}, produces = {"application/json"})
+  public ImageUploadResponse uploadImage(@RequestParam("image") final MultipartFile file) throws IOException {
+    final Path directoryPath = Paths.get(UPLOAD_DIRECTORY, "business-types");
+    Files.createDirectories(directoryPath);
+    final Path fileNameAndPath = directoryPath.resolve(Objects.requireNonNull(file.getOriginalFilename()));
+    logger.info("Saving image to: {}", fileNameAndPath);
+    Files.write(fileNameAndPath, file.getBytes(), StandardOpenOption.CREATE);
+    return new ImageUploadResponse("business-types/" + file.getOriginalFilename());
   }
 
 }
